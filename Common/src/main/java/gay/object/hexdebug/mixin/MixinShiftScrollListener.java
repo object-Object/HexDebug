1ddfdf74eb2272df248cc520fc40092a52c29520
object-Object <object@objectobject.ca>
2024-05-17 21:43:59 -0400

    Fix broken mixin on Fabric

--- Common/src/main/java/gay/object/hexdebug/mixin/MixinShiftScrollListener.java
+++ Common/src/main/java/gay/object/hexdebug/mixin/MixinShiftScrollListener.java
@@ -7,7 +7,11 @@ import gay.object.hexdebug.items.ItemDebugger;
 import gay.object.hexdebug.registry.HexDebugItems;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.item.Item;
+import org.jetbrains.annotations.Contract;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -15,6 +19,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 // client side
 @Mixin(ShiftScrollListener.class)
 public abstract class MixinShiftScrollListener {
+    @Shadow(remap = false)
+    private static double offHandDelta;
+
+    @Contract
+    @Invoker(value = "IsScrollableItem", remap = false)
+    public static boolean hexdebug$invokeIsScrollableItem(Item item) {
+        throw new AssertionError();
+    }
+
     @Inject(method = "IsScrollableItem", at = @At("RETURN"), cancellable = true)
     private static void hexdebug$IsScrollableItem(Item item, CallbackInfoReturnable<Boolean> cir) {
         if (item == HexDebugItems.DEBUGGER.getValue()) {
@@ -22,24 +35,22 @@ public abstract class MixinShiftScrollListener {
         }
     }
 
-    @WrapOperation(
-        method = "onScroll",
-        at = @At(
-            value = "INVOKE",
-            target = "Lat/petrak/hexcasting/client/ShiftScrollListener;IsScrollableItem(Lnet/minecraft/world/item/Item;)Z",
-            ordinal = 0
-        )
-    )
-    private static boolean hexdebug$preferOffhandIfNotDebugging(Item item, Operation<Boolean> original) {
+    // TODO: the duplicated logic here feels nasty, but everything else I tried had issues with remapping
+    @Inject(method = "onScroll", at = @At("HEAD"), cancellable = true, remap = false)
+    private static void hexdebug$preferOffhandIfNotDebugging(double delta, boolean needsSneaking, CallbackInfoReturnable<Boolean> cir) {
         var player = Minecraft.getInstance().player;
         if (
+            // onScroll preconditions
             player != null
+            && (player.isShiftKeyDown() || !needsSneaking)
+            && !player.isSpectator()
+            // additional logic
             && !ItemDebugger.isDebugging()
-            && item == HexDebugItems.DEBUGGER.getValue()
-            && original.call(player.getOffhandItem().getItem())
+            && player.getMainHandItem().getItem() == HexDebugItems.DEBUGGER.getValue()
+            && hexdebug$invokeIsScrollableItem(player.getOffhandItem().getItem())
         ) {
-            return false;
+            offHandDelta += delta;
+            cir.setReturnValue(true);
         }
-        return original.call(item);
     }
 }
