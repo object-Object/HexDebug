9cbd64f2925e965723bf51eb0eafba0e921b6c99
object-Object <object@objectobject.ca>
2024-05-21 15:00:27 -0400

    Add mixin config plugin to handle optional Hexal mixins

---
+++ Common/src/main/java/gay/object/hexdebug/mixin/HexDebugMixinConfigPlugin.java
@@ -0,0 +1,43 @@
+package gay.object.hexdebug.mixin;
+
+import dev.architectury.platform.Platform;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+
+public class HexDebugMixinConfigPlugin implements IMixinConfigPlugin {
+    @Override
+    public void onLoad(String mixinPackage) {}
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        if (mixinClassName.startsWith("gay.object.hexdebug.mixin.hexal")) {
+            return Platform.isModLoaded("hexal");
+        }
+        return true;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {}
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {}
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {}
+}
+
---
+++ Common/src/main/java/gay/object/hexdebug/mixin/hexal/MixinWispCastingManager.java
@@ -0,0 +1,8 @@
+package gay.object.hexdebug.mixin.hexal;
+
+import org.spongepowered.asm.mixin.Mixin;
+import ram.talia.hexal.api.casting.wisp.WispCastingManager;
+
+@Mixin(WispCastingManager.class)
+public class MixinWispCastingManager {
+}
