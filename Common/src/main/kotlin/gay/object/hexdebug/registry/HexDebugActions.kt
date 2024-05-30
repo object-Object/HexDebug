e7af4b9f2b0d09ea38d113a170ffff26e0e48c89
object-Object <object@objectobject.ca>
2024-05-30 16:32:07 -0400

    Add OpNextEvalIndex

--- Common/src/main/kotlin/gay/object/hexdebug/registry/HexDebugActions.kt
+++ Common/src/main/kotlin/gay/object/hexdebug/registry/HexDebugActions.kt
@@ -11,9 +11,11 @@ import at.petrak.hexcasting.common.lib.HexRegistries
 import at.petrak.hexcasting.common.lib.hex.HexActions
 import gay.`object`.hexdebug.casting.actions.OpBreakpoint
 import gay.`object`.hexdebug.casting.actions.OpIsDebugging
+import gay.`object`.hexdebug.casting.actions.OpNextEvalIndex
 
 object HexDebugActions : HexDebugRegistrar<ActionRegistryEntry>(HexRegistries.ACTION, { HexActions.REGISTRY }) {
     val IS_DEBUGGING = make("const/debugging", HexDir.EAST, "qqqqqewaa", OpIsDebugging)
+    val NEXT_EVAL_INDEX = make("next_eval_index", HexDir.SOUTH_WEST, "dedqdeqwaa", OpNextEvalIndex)
 
     val BREAKPOINT_BEFORE = make("breakpoint/before", HexDir.SOUTH_WEST, "awqdeew", OpBreakpoint(true))
     val BREAKPOINT_AFTER = make("breakpoint/after", HexDir.EAST, "wqqaewd", OpBreakpoint(false))
