d4e23d9f065f90343856e7b52edd4c95e8ad44da
object-Object <object@objectobject.ca>
2024-06-18 22:16:33 -0400

    Refactor scrying lens rendering out of FocusHolderBlockEntity

--- Common/src/main/kotlin/gay/object/hexdebug/HexDebugClient.kt
+++ Common/src/main/kotlin/gay/object/hexdebug/HexDebugClient.kt
@@ -1,15 +1,33 @@
 package gay.`object`.hexdebug
 
+import at.petrak.hexcasting.api.casting.iota.IotaType
 import at.petrak.hexcasting.api.client.ScryingLensOverlayRegistry
+import at.petrak.hexcasting.api.utils.asTextComponent
+import at.petrak.hexcasting.api.utils.gray
+import at.petrak.hexcasting.api.utils.plusAssign
 import gay.`object`.hexdebug.adapter.proxy.DebugProxyClient
 import gay.`object`.hexdebug.blocks.focusholder.FocusHolderBlock
 import gay.`object`.hexdebug.config.HexDebugConfig
 import gay.`object`.hexdebug.config.HexDebugConfig.GlobalConfig
 import gay.`object`.hexdebug.registry.HexDebugBlocks
+import gay.`object`.hexdebug.utils.styledHoverName
+import gay.`object`.hexdebug.utils.toComponent
 import me.shedaniel.autoconfig.AutoConfig
+import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.screens.Screen
+import net.minecraft.nbt.CompoundTag
+import net.minecraft.network.chat.Component
+import net.minecraft.network.chat.Style
+import net.minecraft.world.item.Item
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.Items
+import com.mojang.datafixers.util.Pair as MojangPair
+
+typealias ScryingLensLine = MojangPair<ItemStack, Component>
 
 object HexDebugClient {
+    private const val MAX_IOTA_DISPLAY_LINES = 4
+
     fun init() {
         HexDebugConfig.initClient()
         DebugProxyClient.init()
@@ -22,7 +40,47 @@ object HexDebugClient {
 
     private fun addScryingLensOverlays() {
         ScryingLensOverlayRegistry.addDisplayer(HexDebugBlocks.FOCUS_HOLDER.id) { lines, _, pos, _, level, _ ->
-            FocusHolderBlock.getBlockEntity(level, pos)?.addScryingLensLines(lines)
+            FocusHolderBlock.getBlockEntity(level, pos)?.apply {
+                if (iotaStack.isEmpty) return@apply
+
+                // item + name
+                lines += getItemLine(iotaStack)
+
+                // contained iota, if any
+                readIotaTag()?.let {
+                    lines += getIotaLine(Items.PAPER, it)
+                }
+            }
         }
+    }
+
+    private fun getItemLine(stack: ItemStack) = MojangPair(stack, stack.styledHoverName)
+
+    @Suppress("SameParameterValue")
+    private fun getIotaLine(item: Item, tag: CompoundTag) = getIotaLine(ItemStack(item), tag)
+
+    private fun getIotaLine(stack: ItemStack, tag: CompoundTag): ScryingLensLine {
+        val mc = Minecraft.getInstance()
+        val font = mc.font
+
+        // see HexAdditionalRenderers
+        val maxWidth = (mc.window.guiScaledWidth / 2f * 0.8f).toInt()
+
+        val fullDisplay = IotaType.getDisplay(tag)
+        val displayLines = font.splitter.splitLines(fullDisplay, maxWidth, Style.EMPTY)
+
+        val truncatedDisplay = Component.empty()
+        for ((i, line) in displayLines.withIndex()) {
+            if (i > 0) {
+                truncatedDisplay += " ".asTextComponent
+            }
+            if (i >= MAX_IOTA_DISPLAY_LINES) {
+                truncatedDisplay += "...".asTextComponent.gray
+                break
+            }
+            truncatedDisplay += line.toComponent()
+        }
+
+        return MojangPair(stack, truncatedDisplay)
     }
 }
--- Common/src/main/kotlin/gay/object/hexdebug/blocks/focusholder/FocusHolderBlockEntity.kt
+++ Common/src/main/kotlin/gay/object/hexdebug/blocks/focusholder/FocusHolderBlockEntity.kt
@@ -3,29 +3,14 @@ package gay.`object`.hexdebug.blocks.focusholder
 import at.petrak.hexcasting.api.addldata.ADIotaHolder
 import at.petrak.hexcasting.api.block.HexBlockEntity
 import at.petrak.hexcasting.api.casting.iota.Iota
-import at.petrak.hexcasting.api.casting.iota.IotaType
-import at.petrak.hexcasting.api.utils.asTextComponent
-import at.petrak.hexcasting.api.utils.gray
-import at.petrak.hexcasting.api.utils.plusAssign
 import at.petrak.hexcasting.xplat.IXplatAbstractions
 import gay.`object`.hexdebug.blocks.base.BaseContainer
 import gay.`object`.hexdebug.blocks.base.ContainerSlotDelegate
 import gay.`object`.hexdebug.registry.HexDebugBlockEntities
-import gay.`object`.hexdebug.utils.styledHoverName
-import gay.`object`.hexdebug.utils.toComponent
-import net.minecraft.client.Minecraft
 import net.minecraft.core.BlockPos
 import net.minecraft.nbt.CompoundTag
-import net.minecraft.network.chat.Component
-import net.minecraft.network.chat.Style
 import net.minecraft.world.ContainerHelper
-import net.minecraft.world.item.ItemStack
-import net.minecraft.world.item.Items
 import net.minecraft.world.level.block.state.BlockState
-import kotlin.collections.MutableList
-import kotlin.collections.plusAssign
-import kotlin.collections.withIndex
-import com.mojang.datafixers.util.Pair as MojangPair
 
 class FocusHolderBlockEntity(pos: BlockPos, state: BlockState) :
     HexBlockEntity(HexDebugBlockEntities.FOCUS_HOLDER.value, pos, state),
@@ -49,39 +34,4 @@ class FocusHolderBlockEntity(pos: BlockPos, state: BlockState) :
     override fun readIotaTag() = iotaHolder?.readIotaTag()
 
     override fun writeIota(iota: Iota?, simulate: Boolean) = iotaHolder?.writeIota(iota, simulate) ?: false
-
-    fun addScryingLensLines(lines: MutableList<MojangPair<ItemStack, Component>>) {
-        if (iotaStack.isEmpty) return
-
-        lines += MojangPair(iotaStack, iotaStack.styledHoverName)
-
-        readIotaTag()?.let { tag ->
-            val mc = Minecraft.getInstance()
-            val font = mc.font
-
-            // see HexAdditionalRenderers
-            val maxWidth = (mc.window.guiScaledWidth / 2f * 0.8f).toInt()
-
-            val fullDisplay = IotaType.getDisplay(tag)
-            val displayLines = font.splitter.splitLines(fullDisplay, maxWidth, Style.EMPTY)
-
-            val truncatedDisplay = Component.empty()
-            for ((i, line) in displayLines.withIndex()) {
-                if (i > 0) {
-                    truncatedDisplay += " ".asTextComponent
-                }
-                if (i >= MAX_IOTA_DISPLAY_LINES) {
-                    truncatedDisplay += "...".asTextComponent.gray
-                    break
-                }
-                truncatedDisplay += line.toComponent()
-            }
-
-            lines += MojangPair(ItemStack(Items.PAPER), truncatedDisplay)
-        }
-    }
-
-    companion object {
-        const val MAX_IOTA_DISPLAY_LINES = 4
-    }
 }
