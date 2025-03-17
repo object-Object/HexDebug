33216e5acf79b06ca39e99afc28282cd78aed40f
object-Object <object@objectobject.ca>
2025-03-16 20:31:49 -0400

    Remove ContainerDataDelegate and change ContainerDataLongDelegate to use 4 values instead of 2 (fix #23)

--- Common/src/main/kotlin/gay/object/hexdebug/blocks/base/ContainerDataDelegate.kt
+++ Common/src/main/kotlin/gay/object/hexdebug/blocks/base/ContainerDataDelegate.kt
@@ -3,23 +3,28 @@ package gay.`object`.hexdebug.blocks.base
 import net.minecraft.world.inventory.ContainerData
 import kotlin.reflect.KProperty
 
-data class ContainerDataDelegate(val data: ContainerData, val index: Int) {
-    operator fun getValue(thisRef: Any, property: KProperty<*>) = data.get(index)
-
-    operator fun setValue(thisRef: Any, property: KProperty<*>, value: Int) {
-        data.set(index, value)
-    }
-}
-
-data class ContainerDataLongDelegate(val data: ContainerData, val lowIndex: Int, val highIndex: Int) {
+// DataSlot stores ints but serializes them as shorts??????????????????????????????????????????????
+// TODO: idk if this works for negative values but i don't need it to be negative so idc
+data class ContainerDataLongDelegate(
+    val data: ContainerData,
+    val index0: Int,
+    val index1: Int,
+    val index2: Int,
+    val index3: Int,
+) {
     operator fun getValue(thisRef: Any, property: KProperty<*>): Long {
-        val low = data.get(lowIndex).toLong()
-        val high = data.get(highIndex).toLong() shl 32
-        return low + high
+        return (
+            data.get(index0).toUShort().toLong()
+            + (data.get(index1).toUShort().toLong() shl 16)
+            + (data.get(index2).toUShort().toLong() shl 32)
+            + (data.get(index3).toUShort().toLong() shl 48)
+        )
     }
 
     operator fun setValue(thisRef: Any, property: KProperty<*>, value: Long) {
-        data.set(lowIndex, value.toInt())
-        data.set(highIndex, (value shr 32).toInt())
+        data.set(index0, value.toInt())
+        data.set(index1, (value shr 16).toInt())
+        data.set(index2, (value shr 32).toInt())
+        data.set(index3, (value shr 48).toInt())
     }
 }
