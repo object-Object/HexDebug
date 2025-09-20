761828e28071701f9445824f3835d3defe905d4c
object-Object <object@objectobject.ca>
2025-09-20 02:44:33 -0400

    Update Imbue Mindsplice Table cost

--- Common/src/main/kotlin/gay/object/hexdebug/casting/actions/splicing/OpWriteEnlightenedHex.kt
+++ Common/src/main/kotlin/gay/object/hexdebug/casting/actions/splicing/OpWriteEnlightenedHex.kt
@@ -11,7 +11,6 @@ import at.petrak.hexcasting.api.casting.mishaps.MishapBadBlock
 import at.petrak.hexcasting.api.casting.mishaps.MishapOthersName
 import at.petrak.hexcasting.api.misc.MediaConstants
 import gay.`object`.hexdebug.blocks.splicing.SplicingTableBlockEntity
-import net.minecraft.server.level.ServerPlayer
 import net.minecraft.world.phys.Vec3
 
 object OpWriteEnlightenedHex : SpellAction {
@@ -30,14 +29,13 @@ object OpWriteEnlightenedHex : SpellAction {
             throw MishapBadBlock.of(pos, "splicing_table.enlightened")
         }
 
-        val trueName = MishapOthersName.getTrueNameFromArgs(hex, env.castingEntity as? ServerPlayer)
-        if (trueName != null) {
-            throw MishapOthersName(trueName)
+        MishapOthersName.getTrueNameFromArgs(hex, null)?.let {
+            throw MishapOthersName(it)
         }
 
         return SpellAction.Result(
             Spell(table, hex),
-            5 * MediaConstants.CRYSTAL_UNIT,
+            10 * MediaConstants.CRYSTAL_UNIT,
             listOf(ParticleSpray(pos.center, Vec3(1.0, 0.0, 0.0), 0.25, 3.14, 40))
         )
     }
