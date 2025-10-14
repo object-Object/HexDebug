0b6cd67179b79b8b7440bb5c017add5effd670be
object-Object <object@objectobject.ca>
2025-10-14 12:25:21 -0400

    Log warnings instead of crashing when Hexical is outdated (fix #62)

--- Fabric/src/main/kotlin/gay/object/hexdebug/fabric/HexDebugClientAbstractionsImpl.kt
+++ Fabric/src/main/kotlin/gay/object/hexdebug/fabric/HexDebugClientAbstractionsImpl.kt
@@ -2,12 +2,23 @@
 
 package gay.`object`.hexdebug.fabric
 
+import gay.`object`.hexdebug.HexDebug
 import gay.`object`.hexdebug.fabric.interop.FabricHexDebugHexicalInterop
 import net.fabricmc.loader.api.FabricLoader
+import net.fabricmc.loader.api.Version
 import net.minecraft.client.Minecraft
+import kotlin.jvm.optionals.getOrNull
+
+private val HEXICAL_MIN_VERSION: Version = Version.parse("2.0.0")
+private var hasLoggedWarning = false
 
 fun sendHexicalKeyEvent(client: Minecraft, keyCode: Int, scanCode: Int, isPressed: Boolean) {
-    if (FabricLoader.getInstance().isModLoaded("hexical")) {
+    val hexical = FabricLoader.getInstance().getModContainer("hexical").getOrNull() ?: return
+    val hexicalVersion = hexical.metadata.version
+    if (hexicalVersion >= HEXICAL_MIN_VERSION) {
         FabricHexDebugHexicalInterop.sendKeyEvent(client, keyCode, scanCode, isPressed)
+    } else if (!hasLoggedWarning) {
+        HexDebug.LOGGER.error("Hexical splicing table interop requires hexical>=${HEXICAL_MIN_VERSION.friendlyString}, but found ${hexicalVersion.friendlyString}. Disable Hexical interop in HexDebug's client config to suppress this message.")
+        hasLoggedWarning = true
     }
 }
